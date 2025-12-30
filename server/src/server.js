ff8de7ca056ec2491ffdb9bf4a7b203b35270f5a
Mtsegh <mtseghnadooihom@gmail.com>
2025-12-30 15:34:44 +0100

    serverError update

--- server/src/server.js
+++ server/src/server.js
@@ -7,8 +7,11 @@ import authRoutes from "./routes/authRoutes.js"
 import subscribeRoutes from "./routes/subscribeRoute.js"
 import connectDB from "./lib/connectDB.js";
 
-
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config()
 const app = express()
@@ -28,9 +31,9 @@ app.use("/api/subscribe", subscribeRoutes);
 app.use("/api/blogs", blogRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "../../client/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../../client", "dist", "index.html"));
     });
 }
 
