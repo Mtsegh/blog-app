0e62ed869a3d74d5b7fd67676a4114c4c8ccfd52
Mtsegh <mtseghnadooihom@gmail.com>
2026-01-01 17:15:53 +0100

    update navigation after profile update to include user slug

--- client/src/pages/authorPages/EditProfilePage.jsx
+++ client/src/pages/authorPages/EditProfilePage.jsx
@@ -43,7 +43,7 @@ export default function EditProfilePage() {
 
     const success = await updateProfile(updates);
     if (success) {
-      navigate(`/profile`);
+      navigate(`/profile/${authUser.userSlug}`);
     }
   };
 
